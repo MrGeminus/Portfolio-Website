fc588d19d040029a7104ecf05fe4badfb81eeec8
Uros Tomic <urostomic94@gmail.com>
2020-12-19 15:07:54 +0100

    seventh commit

--- php/process.php
+++ php/process.php
@@ -6,13 +6,14 @@ $phone = $_POST['phone'];
 $senderEmail = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+
 $recipientEmail = 'urostomic94@gmail.com';
 $emailBody = "$message";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Type:text/html; charset=UTF-8" . "\r\n";
 $headers .= "From: " .$firstName. " " .$lastName. "<" .$senderEmail. ">" . "\r\n";
 mail ($recipientEmail, $subject, $emailBody, $headers);
-header('Location: ../index.html');
+header('Location: http://mrgeminus.com/');
 exit;
 }
 ?>
