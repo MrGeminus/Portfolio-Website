a1a05b1d20a29eacf0eb8e2b88b51d900e8139f5
Uros Tomic <urostomic94@gmail.com>
2020-12-19 15:46:55 +0100

    eight commit

--- php/process.php
+++ php/process.php
@@ -7,7 +7,7 @@ $senderEmail = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$recipientEmail = 'urostomic94@gmail.com';
+$recipientEmail = 'mrgeaieh@mrgeminus.com';
 $emailBody = "$message";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Type:text/html; charset=UTF-8" . "\r\n";
