72452f47cbafe1f9b428b5f8847f4e1f64f27d44
Uros Tomic <urostomic94@gmail.com>
2020-12-26 10:09:47 +0100

    seventeenth commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
+let contactForm = document.getElementById('contact_form');
 document.addEventListener("DOMContentLoaded", function () {
   // Defining variables
   let sidenav = document.querySelectorAll(".sidenav");
   let scrollspy = document.querySelectorAll('.scrollspy');
   let materialboxed = document.querySelectorAll('.materialboxed');
-  let submitBtn = document.getElementById('submit_btn');
   // Initializing the sidenav
   M.Sidenav.init(sidenav, { edge: 'right' });
   // Initializing the scrollspy
@@ -14,36 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   // Initializing the materialboxed
   M.Materialbox.init(materialboxed, {});
-  submitBtn.addEventListener("click", validateForm);
+  contactForm.addEventListener("submit", validateForm);
 });
 // Validateing the form
 function validateForm(e) {
   e.preventDefault();
-  let firstName = document.getElementById('first_name');
-  let lastName = document.getElementById('last_name');
-  let email = document.getElementById('email');
-  let subject = document.getElementById('subject');
-  let message = document.getElementById('textarea1');
-  let inputFields = [firstName, lastName, email, subject, message];
-  inputFields.forEach(inputField => {
-    let errorMessage = inputField.parentElement.lastElementChild;
-    checkIfFieldIsEmpty(inputField, errorMessage);
-  });
-  checkIfEmailIsValid(inputFields[2]);
-}
-function checkIfFieldIsEmpty(x, y) {
-  if (x.value === '') {
-    x.classList.add('invalid');
-    y.setAttribute("data-error", "This field is required");
-    setTimeout(function () { x.classList.remove('invalid'); y.setAttribute("data-error", "") }, 3500);
-  }
-}
-function checkIfEmailIsValid(x) {
-  let reg = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$");
-  let y = x.parentElement.lastElementChild
-  if (!reg.test(x.value) && !(x.value === '')) {
-    x.classList.add('invalid');
-    y.setAttribute("data-error", "Please enter a valid E-mail");
-    setTimeout(function () { x.classList.remove('invalid'); y.setAttribute("data-error", "") }, 3500);
-  }
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://mrgeminus.com/php/process.php", true);
+  xhr.onload = function () { console.log(xhr.responseText) };
+  xhr.send(new FormData(contactForm));
 }
--- php/process.php
+++ php/process.php
@@ -1,19 +1,16 @@
 <?php
-if(isset($_POST['submit'])){
+header('Access-Control-Allow-Origin: https://mrgeminus.com');
+if(isset($_POST)){
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
-$phone = $_POST['phone'];
 $senderEmail = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-
 $recipientEmail = 'mrgeaieh@mrgeminus.com';
 $emailBody = "$message";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-Type:text/html; charset=UTF-8" . "\r\n";
 $headers .= "From: " .$firstName. " " .$lastName. "<" .$senderEmail. ">" . "\r\n";
 mail ($recipientEmail, $subject, $emailBody, $headers);
-header('Location: http://mrgeminus.com/');
-exit;
 }
 ?>
